c61e5b17e6d9f110c8c8a4b1d9438e96342bedc2
arafatuu <atrahmanbd5@gmail.com>
2021-09-02 18:09:21 +0600

    Laravel setup and practice routing

---
+++ resources/views/greetings.blade.php
@@ -0,0 +1,1 @@
+<h2>This is from Greeetings! How are you?</h2>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+<h1>Hey, This is Arafat Rahman</h1>
+<a href="/greetings">Grereting message</a>
+<a href="/about">About Us</a>
+<a href="/contact">Contact Us</a>
