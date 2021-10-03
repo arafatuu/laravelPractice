2f8cf3b8cd7741f0151be3f9f389056298424293
arafatuu <atrahmanbd5@gmail.com>
2021-10-04 00:00:51 +0600

    MIGRATION

---
+++ database/migrations/2021_10_03_171405_create_test_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->id();
+            $name->string(50);
+            $address->string(50);
+            $table->timestamps();
+        });
+
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test');
+    }
+}
